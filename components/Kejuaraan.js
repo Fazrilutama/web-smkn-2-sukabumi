f4791e2daf8294a2042b6c2a7268952644ef0a19
RafliSky <raflimaulana214@gmail.com>
2023-03-17 11:09:56 +0700

    memperbaiki button baca selengkapnya

--- components/Kejuaraan.js
+++ components/Kejuaraan.js
@@ -12,7 +12,7 @@ export default function Kejuaraan() {
                     Kejuaraan
                 </p>
                 <p className='font-normal text-base text-[#667085] py-2 md:w-[565px] md:mx-auto'>
-                    SMK Negri 2 Sukabumi mendapatkan kejuaraan yang sangat patut di apresiasi dan di banggakan , berikut di antaranya
+                    SMK Negri 2 Sukabumi mendapatkan kejuaraan yang sangat patut di apresiasi dan di banggakan, berikut di antaranya
                 </p>
 
             </div>
@@ -29,7 +29,7 @@ export default function Kejuaraan() {
                                 </p>
                                 <div className=''>
                                     <Link href="https://www.instagram.com/p/Cmf0binPAR6/?utm_source=ig_web_copy_link" target={"_blank"}>
-                                        <button className='bg-[#da251c] py-2 px-6 text-sm font-semibold text-white rounded-[20px] mt-3 '>
+                                        <button className='bg-[#da251c] py-3 px-6 sm:py-4 sm:px-8 text-sm font-semibold text-white rounded-full mt-3 '>
                                             Baca selengkapnya
                                         </button>
                                     </Link>
@@ -49,7 +49,7 @@ export default function Kejuaraan() {
                                     </p>
                                     <div className=''>
                                         <Link href="https://www.instagram.com/dokumenter_smkn2_smi/?hl=id" target={"_blank"}>
-                                            <button className='bg-[#da251c] py-2 px-6 text-sm font-semibold text-white rounded-[20px] mt-3 '>
+                                            <button className='bg-[#da251c] py-3 px-6 sm:py-4 sm:px-8 text-sm font-semibold text-white rounded-full mt-3 '>
                                                 Baca selengkapnya
                                             </button>
                                         </Link>
--- components/Slider.js
+++ components/Slider.js
@@ -8,7 +8,6 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Slider() {
@@ -21,12 +20,12 @@ export default function Slider() {
             >
                 <SwiperSlide>
                     <div className="w-full h-[362px] lg:h-[500px] bg-[url('/images/corosel/mobile1.png')] bg-cover bg-no-repeat md:bg-[url('/images/corosel/dekstop1.png')] md:object-center  ">
-                        <div className=" font-bold text-[28px] lg:text-4xl text-white leading-[33px] lg:leading-[54px] p-6 lg:p-[70px] ">
+                        <div className="font-bold text-[28px] lg:text-4xl text-white leading-[33px] lg:leading-[54px] p-6 lg:p-[70px] ">
                             <p>Upacara</p>
                             <p>Memperingati Hari</p>
                             <p>Pendidikan Nasional</p>
                             <p className="font-normal text-sm lg:text-[28px] pt-1">Bergerak Untuk Merdeka Belajar</p>
-                            <button className="py-2 mt-6 px-4 font-semibold text-xs bg-[#da251c] rounded-full ">
+                            <button className="py-3 px-6 mt-6 sm:py-4 sm:px-8 font-semibold text-sm sm:text-lg bg-[#da251c] rounded-full ">
                                 <Link href="https://www.instagram.com/p/Cdf6e8TvOQk/?utm_source=ig_web_copy_link" target={"_blank"} >Lihat Selengkapnya</Link>
                             </button>
                         </div>
@@ -41,7 +40,7 @@ export default function Slider() {
                             <p>Kabupaten se-</p>
                             <p>Nasional</p>
                             <p className="font-normal text-sm lg:text-[28px] pt-1">Mulia, Profesional, Juara</p>
-                            <button className="py-2 mt-6 px-4 font-semibold text-xs bg-[#da251c] rounded-full ">
+                            <button className="py-3 px-6 sm:py-4 sm:px-8 mt-6 font-semibold text-sm sm:text-lg bg-[#da251c] rounded-full ">
                                 <Link href="https://www.instagram.com/p/Cmf0binPAR6/?utm_source=ig_web_copy_link" target={"_blank"} >Lihat Selengkapnya</Link>
                             </button>
                         </div>
@@ -56,7 +55,7 @@ export default function Slider() {
                             <p>Tingkat Kota Dan</p>
                             <p>Kabupaten Sukabumi</p>
                             <p className="font-normal text-sm lg:text-[28px] pt-1">Mulia, Profesional, Juara</p>
-                            <button className="py-2 mt-6 px-4 font-semibold text-xs bg-[#da251c] rounded-full ">
+                            <button className="py-3 px-6 sm:py-4 sm:px-8 mt-6 font-semibold text-sm sm:text-lg bg-[#da251c] rounded-full ">
                                 <Link href="https://www.instagram.com/p/CldGpEKyg4w/?utm_source=ig_web_copy_link" target={"_blank"} >Lihat Selengkapnya</Link>
                             </button>
                         </div>
